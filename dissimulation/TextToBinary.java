a5f2d0bf6e3ad95c2c45a6771e529e8944fcf4ba
Paul Bertot <paul.bertot@etu.unice.fr>
2015-11-13 16:30:20 +0100

    Nouvelle conversion BitSet, qui remet les bits dans le bon ordre

--- dissimulation/TextToBinary.java
+++ dissimulation/TextToBinary.java
@@ -1,5 +1,4 @@
 package matrice;
-
 import java.util.Arrays;
 import java.util.BitSet;
 
@@ -16,8 +15,19 @@ public class TextToBinary {
 	 */
 	public  BitSet ChaintoBinary(String chaine){
 		byte[] bytes = chaine.getBytes();
+		System.out.println(Arrays.toString(bytes));
 		BitSet bs = BitSet.valueOf(bytes);
-		return bs;
+		BitSet bsret = new BitSet();
+		int z=0;
+		for(int i=0, j=7; j<=bs.length(); i+=8, j+=8 ){
+			BitSet carac = bs.get(i, j);
+			for (int k = 7; k>=0; k--){
+				if(carac.get(k))
+					bsret.set(z);
+				z++;
+			}
+		}
+		return bsret;
 	}
 
 	/**
@@ -45,18 +55,27 @@ public class TextToBinary {
 				binary.append((val & 128) == 0 ? 0 :1);
 				val <<= 1;
 			}
+			binary.append(' ');
 		}
 		return binary.toString();
 	}
 
 	public static void main(String args[]){
-		String s = "Bonjour les amis, vous voulez qqchose à boire?";
+		String s = "bonjour";
 		System.out.println(s);
+		int val;
 		TextToBinary ttb = new TextToBinary();
 		BitSet sb = ttb.ChaintoBinary(s);
-		System.out.println(sb.toString());
-		String res = ttb.BinarytoString(sb);
-		System.out.println(res);
+		for(int i =0; i<=sb.length(); i++){
+			/*if (i%8 ==0){
+				System.out.println("\n");
+			}*/
+			val = sb.get(i)? 1 :0;
+			System.out.print(val);
+			
+		}
+		//String res = ttb.BinarytoString(sb);
+		//System.out.println(res);
 
 	}
 
