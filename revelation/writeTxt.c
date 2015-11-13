c9c9bcca020e4fd45ff8fdf75878546c65e279ea
pol-huang <stephanie1028han@gmail.com>
2015-11-13 14:18:51 +0100

    Read the txt file

---
+++ revelation/writeTxt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	FILE *fp;
+	char ch;
+	if((fp = fopen("text.txt","wt+"))==NULL){
+		printf("Cannot open file\n");
+		getchar();
+		exit(1);
+	}
+	printf("Input the text:\n");
+	ch = getchar();
+	while(ch!='\n'){
+		fputc(ch,fp);
+		ch = getchar();
+	}
+	rewind(fp);
+	ch = fgetc(fp);
+	while(ch!=EOF){
+		putchar(ch);
+		ch = fgetc(fp);
+	}
+	printf("\n");
+	fclose(fp);
+}
