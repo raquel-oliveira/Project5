83d7f2063bc85a4219f49a765f8ef481ef81c550
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2015-11-16 10:47:02 +0100

    Update CMake files

---
+++ revelation/CMakeFiles/Revelation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Revelation.dir/src/main.c.o"
+  "bin/Revelation.pdb"
+  "bin/Revelation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Revelation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
