c7a704ed7403f6d105486f2ba49cab6a11a80cd0
Charly Lafon <charlylafon@gmail.com>
2015-11-18 16:04:29 +0100

    PSM-12 Ajout de commentaires

--- dissimulation/Matrice/src/matrice/Main.java
+++ dissimulation/Matrice/src/matrice/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		Dissimulation d = new Dissimulation(img);
 		TextToBinary t = new TextToBinary();
 		
-		BitSet b = t.ChaintoBinary("Bonjour");
+		BitSet b = t.ChaintoBinary("Comment ca va ?");
 		
 		manipMat.getPixelsColor("red");
 		manipMat.getPixelsColor("blue");
@@ -21,13 +21,5 @@ public class Main {
 		d.dissimulationLSB(b);
 		
 		manipMat.setPixelsColor();
-	
-		
-		for(int i = 0; i < img.getRedArray().length; i++)
-		{
-			System.out.println(img.getRedArray()[i]);
-			System.out.println(img.getGreenArray()[i]);
-			System.out.println(img.getBlueArray()[i]);
-		}
 	}
 }
---
+++ dissimulation/Matrice/src/matrice/TextToBinary.java
@@ -0,0 +1,63 @@
+package matrice;
+import java.util.Arrays;
+import java.util.BitSet;
+
+
+
+public class TextToBinary {
+
+	/**
+	 * This method takes a string as an argument, 
+	 * and returns a string containing the binary values of the characters of the string. 
+	 * 
+	 * @param chaine
+	 * @return BitSet
+	 */
+	public  BitSet ChaintoBinary(String chaine){
+		byte[] bytes = chaine.getBytes(); //réccupération des bytes correspondant a la chaine de characères.
+		//System.out.println(Arrays.toString(bytes));
+		BitSet bs = BitSet.valueOf(bytes); //Passage de byte[] à BitSet (une liste de valeurs booléennes)
+		BitSet bsret = new BitSet();
+		int z=0;
+		for(int i=0, j=7; j<=bs.length(); i+=8, j+=8 ){ //Le bitset ne mettant pas les bits dans l'ordre naturel de lecture, on les inverse 8 par 8
+			BitSet carac = bs.get(i, j);
+			for (int k = 7; k>=0; k--){
+				if(carac.get(k))
+					bsret.set(z);
+				z++;
+			}
+		}
+		return bsret;
+	}
+
+	/**
+	 * This method takes a byte Array
+	 * and returns a  string.
+	 * 
+	 * 
+	 * @param BitSet
+	 * @return text
+	 */
+	public String BinarytoString(BitSet bs){
+		String ret;
+		byte[] bc = bs.toByteArray();
+		ret = new String(bc);
+		return ret;
+
+	}
+
+	public String StringtoBinaryString(String s){
+		byte[] bytes = s.getBytes();
+		StringBuilder binary = new StringBuilder();
+		for(byte b : bytes){
+			int val = b;
+			for(int i =0; i<8; i++){
+				binary.append((val & 128) == 0 ? 0 :1);
+				val <<= 1;
+			}
+			binary.append(' ');
+		}
+		return binary.toString();
+	}
+
+}
