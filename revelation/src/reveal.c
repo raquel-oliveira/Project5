0bd0323ea7b7d4bb1fcd57d864f3ed0beecbe637
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2015-12-04 17:52:34 +0100

    change of way to use the b parameter

--- revelation/src/reveal.c
+++ revelation/src/reveal.c
@@ -5,22 +5,25 @@
  *  @param help - define de ends of the message
  */
 uchar* revealDirect(IplImage *img, int b, char* help, uchar* message){
-    CvScalar pixel;
-    int lbit, channel;
-    int count = 7;
-    uchar letter = 1;
+    int lbit, channel; int count = 7; int i = 0;
+    uchar letter = 1; char* end;
     message = malloc(1000); //change this
-    char* end;
-    int i = 0;
     for (int row = 0; row < img->height; row++){
         for (int col = 0; col < img->width; col++){
             channel = cvGet2D(img, row, col).val[2]; //channel red
-            lbit = get_bit(channel, b); //red, last bit if b = 1;
-            letter = setBit(letter, count, lbit);
-            count--;
-            if (count < 0){ count = 7; message[i++] = letter; }
-                end = strstr((char*) message, help);
-                if (end != 0){ *end = '\0'; return message; }
+                lbit = get_bit(channel, (9 - b)); //access to the bit in ascending order
+                letter = setBit(letter, count, lbit);
+                count--;
+                if (count < 0) {
+                    count = 7;
+                    message[i++] = letter;
+                }
+
+                end = strstr((char *) message, help);
+                if (end != 0) {
+                    *end = '\0';
+                    return message;
+                }
         }
     }
     return message;
@@ -50,7 +53,7 @@ int main() {
     channels  = getChannels(img);
 
 
-    message = revealDirect(img,8,help, message);
+    message = revealDirect(img,1,help, message);
     int i = 0;
     while(message[i] != '\0'){
         printf("%c", message[i]);
