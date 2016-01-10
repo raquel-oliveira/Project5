4adc8eb24cc77a98d1cae8954dbaea7c9a88b092
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-01-11 00:31:17 +0100

    bash e

--- dissimulation/build.sh
+++ dissimulation/build.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/sh
 # to run: $ . file.sh
 file="./build.sh"
@@ -16,14 +15,3 @@ then
 else
    echo "-- error"
 fi
-=======
-#!/bin/bash
-
-SRCFOLDER=$(pwd)/src
-CLASSPATH=$(pwd)/classes
-cd $SRCFOLDER
-javac -d $CLASSPATH dissimulator/Main.java
-
-
-jar cmf $SRCFOLDER/META-INF/MANIFEST.MF dissimulate.jar -C $CLASSPATH
->>>>>>> 1d6eceb8b02585b5b61491cb530fb92cc1cf3dfc
