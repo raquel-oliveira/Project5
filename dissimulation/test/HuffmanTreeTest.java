987be60dbcb0078149bc544aba971d4bea1c50d5
Paul Bertot <paul.bertot@etu.unice.fr>
2016-01-18 12:53:33 +0100

    Modifications test

--- dissimulation/test/HuffmanTreeTest.java
+++ dissimulation/test/HuffmanTreeTest.java
@@ -9,12 +9,22 @@ import static org.junit.Assert.assertTrue;
  */
 public class HuffmanTreeTest {
     @Test
-    public void HuffmanTreeTest(){
+    public void HuffmanTreeCreateTest(){
         HuffmanTree htt = new HuffmanTree();
-        String occ = "{c:1, b:2, d:3, e:3, a:4}";
+        String occ = "c:1,b:2,d:3,e:3,a:4";
         htt.createList(occ);
 
        assertEquals("c:1", htt.getTree().get(0));
         assertEquals("d:3", htt.getTree().get(2));
+    }
+
+    public void HuffmanTreeAddTest(){
+        HuffmanTree htt = new HuffmanTree();
+        String occ = "c:1,b:2,d:3,e:3,a:4";
+        htt.createList(occ);
+        assertEquals("d:3", htt.getTree().get(2));
+
+        htt.addNode("f:2");
+        assertEquals("f:2", htt.getTree().get(2));
     }
 }
