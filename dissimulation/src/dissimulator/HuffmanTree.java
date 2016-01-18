749148e318c028660acbbfc4720820c444a2f3f8
Paul Bertot <paul.bertot@etu.unice.fr>
2016-01-18 11:28:49 +0100

    Creation de la classe HuffmanTree qui travaille sur les regroupements de node

---
+++ dissimulation/src/dissimulator/HuffmanTree.java
@@ -0,0 +1,53 @@
+package dissimulator;
+
+/**
+ * Created by paul Bertot on 1/18/16.
+ */
+import java.util.LinkedList;
+
+
+public class HuffmanTree {
+    private LinkedList<String> tree;
+
+    public HuffmanTree(){
+        this.tree = new LinkedList<String>();
+    }
+
+    public LinkedList<String> getTree(){
+        return this.tree;
+    }
+
+    public void addNode(String node){
+        int index = 0;
+        String[] nodeS = node.split(":");
+        int occ = Integer.parseInt(nodeS[1]);
+        if(!this.getTree().isEmpty()){
+            for (String n : this.getTree()){
+                String[] nS = n.split(":");
+                if (occ >= Integer.parseInt(nS[1]))
+                    index++;
+            }
+            this.getTree().add(index, node);
+        }
+        else this.getTree().add(node);
+    }
+
+    public void fuseNodes(){
+        StringBuilder sb = new StringBuilder("(,)");
+        String n1 = this.getTree().get(0);
+        String n2 = this.getTree().get(1);
+        String[] n1split = n1.split(":");
+        String[] n2split = n2.split(":");
+
+        int occ = Integer.parseInt(n1split[1]) + Integer.parseInt(n2split[1]);
+
+        sb.insert(1, n1split[0]);
+        sb.insert(n1split[0].length()+2, n2split[0]);
+        sb.append(":"+occ);
+        this.addNode(sb.toString());
+        this.getTree().remove();
+        this.getTree().remove();
+    }
+
+
+}
