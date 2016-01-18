d60fa432cdca0c5ec9694d7194e3444b0d14190d
Paul Bertot <paul.bertot@etu.unice.fr>
2016-01-18 14:08:59 +0100

    Ajout récursivité pour déterminer les codes correspondant aux lettres (entiers)

--- dissimulation/src/dissimulator/HuffmanTree.java
+++ dissimulation/src/dissimulator/HuffmanTree.java
@@ -3,39 +3,49 @@ package dissimulator;
 /**
  * Created by paul Bertot on 1/18/16.
  */
+import java.util.HashMap;
 import java.util.LinkedList;
 
 
 public class HuffmanTree {
-    private LinkedList<String> tree;
+    private LinkedList<String> nodes;
+    private String tree;
 
     public HuffmanTree(){
-        this.tree = new LinkedList<String>();
+        this.nodes = new LinkedList<String>();
     }
 
-    public LinkedList<String> getTree(){
+    public String getTree(){
         return this.tree;
+    }
+
+    public void setTree(String s){
+        this.tree = s;
+    }
+
+    public LinkedList<String> getNodes(){
+        return this.nodes;
     }
 
     public void addNode(String node){
         int index = 0;
         String[] nodeS = node.split(":");
         int occ = Integer.parseInt(nodeS[1]);
-        if(!this.getTree().isEmpty()){
-            for (String n : this.getTree()){
+        if(!this.getNodes().isEmpty()){
+            for (String n : this.getNodes()){
                 String[] nS = n.split(":");
                 if (occ >= Integer.parseInt(nS[1]))
                     index++;
             }
-            this.getTree().add(index, node);
+            this.getNodes().add(index, node);
         }
-        else this.getTree().add(node);
+        else this.getNodes().add(node);
     }
 
     public void fuseNodes(){
         StringBuilder sb = new StringBuilder("(,)");
-        String n1 = this.getTree().get(0);
-        String n2 = this.getTree().get(1);
+        String n1 = this.getNodes().get(0);
+        String n2 = this.getNodes().get(1);
         String[] n1split = n1.split(":");
         String[] n2split = n2.split(":");
 
@@ -45,16 +55,47 @@ public class HuffmanTree {
         sb.insert(n1split[0].length()+2, n2split[0]);
         sb.append(":"+occ);
         this.addNode(sb.toString());
-        this.getTree().remove();
-        this.getTree().remove();
+        this.getNodes().remove();
+        this.getNodes().remove();
     }
 
     public void createList(String occurences){
-        String[] tuples = occurences.split(",");
+        String occur = occurences.substring(1, occurences.length()-1);
+        String[] tuples = occur.split(", ");
         for(int i=0; i < tuples.length; i++){
             this.addNode(tuples[i]);
         }
 
+    }
+
+    public int comaPosition(String s){
+        int pos =0;
+        int alt = 0;
+        for(int i=0; i< s.length(); i++){
+            if(s.charAt(i)=='(')
+                alt++;
+            else if (s.charAt(i) ==')')
+                alt--;
+            else if(s.charAt(i) == ',' && alt ==0)
+                pos = i;
+        }
+        return pos;
+    }
+
+    public HashMap<String, Integer> getCodes(){
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        this.getCodes(hm, this.getTree(), 0);
+
+        return hm;
+    }
+
+    private void getCodes(HashMap<String, Integer> hash, String node, int value){
+        if (node.length()>1){
+            node = node.substring(1, node.length()-1);
+            this.getCodes(hash, node.substring(0, this.comaPosition(node)), value*2+0);
+            this.getCodes(hash, node.substring(this.comaPosition(node)+1, node.length()), value*2+1);
+        }
+        else hash.put(node, value);
     }
 
 
