52df792f16c3d00c20a3faea448a9dbe9aeba804
Charly Lafon <charlylafon@gmail.com>
2016-01-19 10:10:13 +0100

    Improveming metrics using PSM-47

--- dissimulation/src/dissimulator/Metrics.java
+++ dissimulation/src/dissimulator/Metrics.java
@@ -2,6 +2,7 @@ package dissimulator;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.BitSet;
 
 public class Metrics {
 
@@ -31,5 +32,19 @@ public class Metrics {
 	{
 		end = Instant.now();
 		System.out.println(Duration.between(start, end).toMillis());
+	}
+	
+	/**
+	 * Calculates then prints out the space saved by the compression. The result may be below zero, showing that the compression
+	 * was not useful.
+	 */
+	public void getCompressionSavings(String originalMessage, BitSet compressedMessage)
+	{
+		int sizeOriginal = originalMessage.length() * 8;
+		int sizeCompressed = compressedMessage.length();
+		
+		int sizeSaved = 1 - (sizeCompressed/sizeOriginal)*100;
+		
+		System.out.println("Saved space with compression : " + sizeSaved + "%");
 	}
 }
