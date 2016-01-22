6d1e89799d0c0c7c063213e290c4958f29b4238b
Charly Lafon <charlylafon@gmail.com>
2016-01-22 11:19:14 +0100

    Adding of tests PSM-57

--- dissimulation/src/test/MetricsTests.java
+++ dissimulation/src/test/MetricsTests.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import dissimulator.Metrics;
 import java.util.BitSet;
+import java.util.HashMap;
 
 public class MetricsTests {
 
@@ -19,6 +20,11 @@ public class MetricsTests {
 	}
 	
 	@Test
+	/**
+	 * Verify if the compression_saving calculation is right. 
+	 * Here, the compressed message is much longer than
+	 * the original one. The result must be negative.
+	 */
 	public void compressionSavingsTest()
 	{
 		String originalMessage = "abc";
@@ -27,6 +33,20 @@ public class MetricsTests {
 		
 		double sizeSaved = met.getCompressionSavings(originalMessage, compressedMessage);
 		assertTrue(sizeSaved < 0);
+	}
+	
+	@Test
+	/**
+	 * Verify if the compression_saving calculation is right. 
+	 * Here, the compressed message is much longer than
+	 * the original one. The result must be negative.
+	 */
+	public void getDictionaryTest()
+	{
+		HashMap<String, Integer> dicoCode = new HashMap<>();
+		HashMap<String, Integer> dicoLength = new HashMap<>();
+		
+		
 	}
 
 }
